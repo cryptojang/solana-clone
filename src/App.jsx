bb4492dc8b2bdf55575ba778ea7593a5c5beac77
cryptojang <robert.crypto.jang@gmail.com>
2023-11-20 17:07:05 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <>
+      <header className="bg-red-100 h-[67.3px]">sticky header</header>
+      <div className="bg-orange-100 h-[745.78px]">Powerful for Developers</div>
+      <div className="bg-yellow-100 h-[82.09px]">partners</div>
+      <div className="bg-green-100 h-[336.09px]">banner</div>
+      <div className="bg-blue-100">join</div>
+      <div className="bg-purple-100">case studies</div>
+      <div className="bg-red-100">build for growth</div>
+      <div className="bg-orange-100">join a thriving community</div>
+      <footer className="bg-yellow-100">footer</footer>
+    </>
+  );
 };
 
 export default App;
